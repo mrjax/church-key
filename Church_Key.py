d361b181310f30472b5bfc4e1d5d854f7725bfc0
mrjax <quesomonos@gmail.com>
2015-09-09 09:54:03 -0500

    Initial commit
    Should be functional.  Have to test package control updating

---
+++ Church_Key.py
@@ -0,0 +1,84 @@
+
+import sublime, sublime_plugin
+import os, sys, webbrowser, urllib
+
+class ChurchKeyCommand(sublime_plugin.TextCommand):
+	config = sublime.load_settings("Church_Key.sublime-settings")
+	
+	def run(self, edit):
+		dir = self.config.get("homeDirectory")
+		spaces = self.config.get("spaceLimit")
+
+		#getting input with no spaces
+		input = self.selection(0)
+
+		#Main Structure below, mostly an altered If-ifelse-else
+		#if -> use single-word (no spaces) input as a url to open?
+		#ifelse -> loop through number of spaces to attempt to discover a file
+		#else -> use test indicator "escape" to determine that no file was opened
+		#		 and thus the selection should be googled
+		domains = self.config.get("domainList")
+		if any(dom in input for dom in domains): 
+			webbrowser.open_new_tab(input)
+		else:
+			index = 0
+			escape = False
+			while(index <= int(spaces) and escape == False):
+				escape = self.openFile(self.selection(index))
+				index += 1
+
+		if escape == False:
+			webbrowser.open_new_tab("http://google.com/#q=" + urllib.quote(input))
+
+	def selection(self, limit):
+		s = self.view.sel()[0]
+
+		start = s.a
+		end = s.b
+		size = self.view.size()
+		count = 0
+
+		if (start == end):
+			while(start > 0 and self.view.substr(start - 1) != ' '
+							and self.view.substr(start - 1) != '\n'):
+				start -= 1
+
+			while(end < size and self.view.substr(end) != '\n'):
+				end += 1
+				if self.view.substr(end) == ' ':
+					if count == limit:
+						break;
+					count += 1
+
+		return self.view.substr(sublime.Region(start, end)).strip()
+
+	def openFile(self, input):
+		dir = self.config.get("homeDirectory")
+		escape = False
+
+		if os.path.isdir(input):
+			#this is an absolute path to a directory
+			os.startfile(input)
+			escape = True
+		elif os.path.isfile(input):
+			#this is an absolute path to a file
+			os.startfile(input)
+			escape = True
+		elif os.path.isdir(os.path.normpath(dir + "\\" + input)):
+			#this is a relative dir in config.homeDirectory  /projects/ 
+			os.startfile(os.path.normpath(dir + "\\" + input))
+			escape = True
+		elif os.path.isfile(os.path.normpath(dir + "\\" + input)):
+			#this is a relative file in config.homeDirectory
+			os.startfile(os.path.normpath(dir + "\\" + input))
+			escape = True
+		elif os.path.isdir(os.path.normpath(os.path.dirname(self.view.file_name()) + "\\" + input)):
+			#this is a relative dir in directory of selection
+			os.startfile(os.path.normpath(os.path.dirname(self.view.file_name()) + "\\" + input))
+			escape = True
+		elif os.path.isfile(os.path.normpath(os.path.dirname(self.view.file_name()) + "\\" + input)):
+			#this is a relative file in directory of selection
+			os.startfile(os.path.normpath(os.path.dirname(self.view.file_name()) + "\\" + input))
+			escape = True
+
+		return escape
---
+++ README.md
@@ -0,0 +1,33 @@
+# church-key
+Opens relative and absolute files and URLs in Sublime Text 2
+
+
+Given the following folder/file structure:
+C:/Users/user/Desktop/TestFolder
+...../subFolder
+..........file.txt
+..........2nd file.txt
+...../2nd subFolder
+..........myJam.mp3
+.....siblingFile.txt
+.....photo.jpg
+.....youAreHere.txt
+
+Where you have youAreHere.txt open,..
+
+Click in this:...			and [ctrl + u] will:...				Because it is..
+C:/Users/User/Desktop		Open folder in Explorer				Absolute path to directory
+siblingFile.jpg 			Open file in notepad/Sublime		Relative path to Within containing folder of current file
+/photo.jpg 					Open image in Photo Viewer			Link works with/without extra "/"s
+/subFolder					Open folder in Explorer				Relative path to directory within containing folder of current file
+/subFolder/file.txt 		Open file in notepad/Sublime 		Relative path works
+/subFolder/2nd file.txt 	Open file in notepad/Sublime		Folder name will accept 2 following spaces, see Config Section
+
+
+
+
+explain spaces work after cursor
+explain from noacad/open-url
+
+
+
